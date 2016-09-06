61dff1e4f63017d2ee2a14fc41fe8777bfde00a3
faustoinfausto <scama@pucp.pe>
2016-09-06 17:37:17 -0500

    prueba1
    prueba1

--- src/Controllers/Dibujador.java
+++ src/Controllers/Dibujador.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Probando
 package Controllers;
 import Models.Dungeon;
 import Models.Avatar;
