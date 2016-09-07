4b35146fc461e1fc770bfd3fdbbb1af01ac6f030
faustoinfausto <scama@pucp.pe>
2016-09-06 22:57:56 -0500

    solo comentarios
    solo comentarios

--- src/Models/Avatar.java
+++ src/Models/Avatar.java
@@ -141,114 +141,116 @@ public class Avatar extends Entity implements IDibujable{
     }
     
     //Modificado por mi
-    public void Render(){
-        //El nuevo render imprime 4 lineas por separado y el resto en un for
-        String space20 = new String(new char[22]).replace('\0', ' ');
+    public void Render() { //El nuevo render imprime 4 lineas por separado y el resto en un for
+
+        String space20 = new String(new char[22]).replace('\0', ' '); //Cadenas de espacios en blanco
         String space19 = new String(new char[21]).replace('\0', ' ');
         int tamanho = saco.size();
+
         
+        //Cabeceras
         System.out.println("");
-        System.out.format("%-40s","     ------ STATS ------");
-        //Cadenas de espacios en blanco
+        System.out.format("%-40s", "     ------ STATS ------");
         System.out.print(space20);
         System.out.println("---------------- ITEMS -----------------");
-              
-        //Stats
-        System.out.format("%-40s","     "+this.GetNombre());
-        
-        //Arma
-         if (tamanho == 0){
-             //Cadenas de espacios en blanco
+
+        //La primera linea
+        System.out.format("%-40s", "     " + this.GetNombre()); //Imprime el nombre
+
+       if (tamanho == 0) { //Si no hay items 
             System.out.print(space20);
-            System.out.format("%-40s\n",". . . . . . . . .Vacio. . . . . . . . . ");
-        }else{             
-             if(!(saco.get(0)==arma_equip)&&!(saco.get(0)==armadura_equip)) System.out.print(space20);
-             else 
-             {
-                 //Cadenas de espacios en blanco
-                System.out.print(space19);                
-                System.out.print(">");
-             }
-             saco.get(0).Render();    
-             System.out.format("%10s","(1)\n");
-             
-        }        
-        
-        System.out.format("%-20s%-7d / %-8d  ","     HP: ",this.hp,this.vidaMaxima);
-        
-        if(tamanho>=2)
-        {
-           
-            if(!(saco.get(1)==arma_equip)&&!(saco.get(1)==armadura_equip)) System.out.print(space20);
-            else 
-            {                 
+            System.out.format("%-40s\n", ". . . . . . . . .Vacio. . . . . . . . . ");
+        } else { //Si hay items
+            if (!(saco.get(0) == arma_equip) && !(saco.get(0) == armadura_equip)) { //Si no esta equipado ese item
+                System.out.print(space20);
+            } else { // Si si lo esta imprime un >
                 System.out.print(space19);
                 System.out.print(">");
             }
-            saco.get(1).Render();    
-            System.out.format("%10s","(2)\n");
-        }else{
-             System.out.printf("\n");
+            saco.get(0).Render(); //Muestra el item y luego coloca el indice
+            System.out.format("%10s", "(1)\n");
+
+        } //Es la misma logica para todos los bloques if que vienen a continuacion
+
+        System.out.format("%-20s%-7d / %-8d  ", "     HP: ", this.hp, this.vidaMaxima); //Imprime la vida
+
+        if (tamanho >= 2) {
+
+            if (!(saco.get(1) == arma_equip) && !(saco.get(1) == armadura_equip)) {
+                System.out.print(space20);
+            } else {
+                System.out.print(space19);
+                System.out.print(">");
+            }
+            saco.get(1).Render();
+            System.out.format("%10s", "(2)\n");
+        } else {
+            System.out.printf("\n");
         }
-       
-        
-        System.out.printf("     Arma: ");
-        if (arma_equip == null) System.out.format("%-29s","Ninguno"); else arma_equip.Render();
-        
-        if(tamanho>=3)
-        {           
-            if(!(saco.get(2)==arma_equip)&&!(saco.get(2)==armadura_equip)) System.out.print(space20);
-             else 
-             {                 
-                System.out.print(space19);                
-                System.out.print(">");
-             }
-            saco.get(2).Render();       
-            System.out.format("%10s","(3)\n");
-        }else{
-             System.out.printf("\n");
-        }       
-        
-        System.out.print("     Armadura: ");
-        if (armadura_equip == null) System.out.format("%-40s","Ninguno"); else armadura_equip.Render(); 
-        Artefacto art;
-        
-         if(tamanho>=4)
-        {            
-            if(!(saco.get(3)==arma_equip)&&!(saco.get(3)==armadura_equip)) System.out.print(space20);
-             else 
-             {                 
+
+        System.out.printf("     Arma: "); //Imprime el arma equipada
+        if (arma_equip == null) { // Si no tiene nada equipado
+            System.out.format("%-29s", "Ninguno");
+        } else {
+            arma_equip.Render();
+        }
+
+        if (tamanho >= 3) {
+            if (!(saco.get(2) == arma_equip) && !(saco.get(2) == armadura_equip)) {
+                System.out.print(space20);
+            } else {
                 System.out.print(space19);
                 System.out.print(">");
-             }
-            saco.get(3).Render(); 
-            System.out.format("%10s","(4)\n");
-        }else{
-             System.out.printf("\n");
-        }                               
-        
-        for (int i= 4; i <tamanho ; i++){
-            art = saco.get(i);
-            System.out.format("%-30s"," ");           
-            if(!(saco.get(i)==armadura_equip)&&!(saco.get(i)==armadura_equip)) System.out.print(space20);
-            else 
-            {                 
-                System.out.print(space19);                
+            }
+            saco.get(2).Render();
+            System.out.format("%10s", "(3)\n");
+        } else {
+            System.out.printf("\n");
+        }
+
+        System.out.print("     Armadura: "); //Ahora la armadura
+        if (armadura_equip == null) { // Si no tiene nada equipado
+            System.out.format("%-40s", "Ninguno");
+        } else {
+            armadura_equip.Render();
+        }
+        Artefacto art; //Declaracion aleatoria salvaje aparecio :v
+
+        if (tamanho >= 4) {
+            if (!(saco.get(3) == arma_equip) && !(saco.get(3) == armadura_equip)) {
+                System.out.print(space20);
+            } else {
+                System.out.print(space19);
                 System.out.print(">");
             }
-            art.Render();
-            System.out.format("%8s%d)","(",i+1);
+            saco.get(3).Render();
+            System.out.format("%10s", "(4)\n");
+        } else {
+            System.out.printf("\n");
+        }
+
+        for (int i = 4; i < tamanho; i++) { //El for que imprime de la linea 5 para adelante
+            art = saco.get(i); //El item del saco en la posicion i
+            System.out.format("%-30s", " "); //Espacios en blanco
+            if (!(saco.get(i) == armadura_equip) && !(saco.get(i) == armadura_equip)) { //Lo mismo de antes
+                System.out.print(space20);
+            } else {
+                System.out.print(space19);
+                System.out.print(">");
+            }
+            art.Render(); //Imprime los datos del indice
+            System.out.format("%8s%d)", "(", i + 1); //Imprime el indice
             System.out.printf("\n");
             //System.out.println();
         }
     }
-    
-    public void LoadComponents(){
-        
+
+    public void LoadComponents() {
+
     }
-    
-    public void Dispose(){
-        
+
+    public void Dispose() {
+
     }
-    
+
 }
